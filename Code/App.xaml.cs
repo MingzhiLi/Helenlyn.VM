93a622c3b70538206edce6e1322e79fbbcefbfc0
翁智华 <374532@nd.com>
2018-10-16 19:26:00 +0800

    VM、Messenger、DataBind

---
+++ Code/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace MVVMLightDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {          
+           this.InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 添加启动参数的处理
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+        }
+    }
+}
